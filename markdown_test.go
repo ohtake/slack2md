ad5235151145995bfe7a5e23ab29d80877ed6fe5
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2016-07-20 14:16:12 +0900

    Add more test

--- markdown_test.go
+++ markdown_test.go
@@ -93,5 +93,14 @@ func TestMarkdownMessageList(t *testing.T) {
 }
 
 func TestMarkdownUserTable(t *testing.T) {
-	// TODO
+	users := ReadUsers("test_data/users.json")
+	m := NewMarkdownTranslator()
+	actual := m.ToUserTable(users)
+	testLines(t, actual, []string{
+		"|ID|Icon|Name|Email|FirstName|LastName|Title|",
+		"|----|----|----|----|----|----|----|",
+		"|U00000001|![](https://avatars.slack-edge.com/2016-04-27/00000000000_01234567890abcdef012_24.jpg)|alice|alice.doe@example.com|Alice|Doe|title1|",
+		"|U00000002|![](https://secure.gravatar.com/avatar/0123456789abcdef0123456789abcdef.jpg?s=24&d=https%3A%2F%2Fa.slack-edge.com%2F66f9%2Fimg%2Favatars%2Fava_0002-24.png)|bob|bob.doe@example.com|Bob|Doe|title2|",
+		"",
+	})
 }
