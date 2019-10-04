842e0caf70dbf6e7e5ae248df40c36dda0f1ad85
OHTAKE Tomohiro <ohtake.tomohiro@jp.fujitsu.com>
2019-10-04 09:50:00 +0900

    Use dist directory for distribution

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,8 +3,8 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-mkdir -p out
+mkdir -p dist
 
-GOOS=darwin  GOARCH=amd64 go build -o out/slack2md.darwin-amd64.out
-GOOS=linux   GOARCH=amd64 go build -o out/slack2md.linux-amd64.out
-GOOS=windows GOARCH=amd64 go build -o out/slack2md.windows-amd64.exe
+GOOS=darwin  GOARCH=amd64 go build -o dist/slack2md.darwin-amd64.out
+GOOS=linux   GOARCH=amd64 go build -o dist/slack2md.linux-amd64.out
+GOOS=windows GOARCH=amd64 go build -o dist/slack2md.windows-amd64.exe
